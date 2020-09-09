203708b6a36af1eb1bcf01cc4c88e74f81abb4e2
tranngocson210599 <dp1.tranngocson99@gmail.com>
2020-09-09 21:04:44 +0700

    Modify Queue.h and Queue.cpp

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,9 +1,9 @@
 #include"Queue.h"
 using namespace std;
 
-void CreateQueue(Queue &q,int size)
- {
-    q.data = new int[size];
+void CreateQueue(Queue& q, int size)
+{
+	q.data = new int[size];
 	q.capacity = size;
 	q.front = 0;
 	q.rear = -1;
@@ -11,26 +11,26 @@ void CreateQueue(Queue &q,int size)
 }
 
 
-int size(Queue &q)
+int size(Queue& q)
 {
-return q.count;
+	return q.count;
 }
 
 
 
-bool isFull(Queue &q)
+bool isFull(Queue& q)
 {
- return (size(q) == q.capacity);
+	return (size(q) == q.capacity);
 }
 
- bool isEmpty(Queue &q)
- {
-     return (size(q) == 0);
- }
+bool isEmpty(Queue& q)
+{
+	return (size(q) == 0);
+}
 
- int EnQueue(Queue &q, int x) 
- {
-if (isFull(q))
+int  EnQueue(Queue& q, int x)
+{
+	if (isFull(q))
 	{
 		cout << "Queue is Full";
 		return 0;
@@ -41,9 +41,9 @@ if (isFull(q))
 }
 
 
- int DeQueue(Queue &q, int &x) 
- {
-if (isEmpty(q))
+int DeQueue(Queue& q)
+{
+	if (isEmpty(q))
 	{
 		cout << "Queue is Empty";
 		return 0;
@@ -51,11 +51,36 @@ if (isEmpty(q))
 	q.front = (q.front + 1) % q.capacity;
 	q.count--;
 }
- int Peek(Queue &q)
- {
-     if (isEmpty(q))
+int Peek(Queue& q)
+{
+	if (isEmpty(q))
 	{
-		cout<<"Queue is Empty"<<endl;
+		cout << "Queue is Empty" << endl;
 	}
 	return q.data[q.front];
- }
+}
+void Print(Queue q)
+{
+	for (int i = 0; i < q.count; i++)
+	{
+		int index = (q.front + i) % q.capacity;
+		cout << q.data[index] << endl;
+	}
+
+}
+//selection sort
+void Sort(Queue& q)
+{
+	int i, j, min_idx;
+	for (i = q.front; i < q.rear - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < q.rear; j++)
+		{
+			if (q.data[min_idx] > q.data[j])
+				min_idx = j;
+		}
+		swap(q.data[i], q.data[min_idx]);
+	}
+
+}
