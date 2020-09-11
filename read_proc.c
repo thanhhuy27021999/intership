650f23d271a10224fe156481dd7e1c21c070e56e
thanhhuy27021999 <thanhhuy27021999@gmail.com>
2020-09-11 16:13:46 +0700

    Listting some running process including: Name, Pid, mems, Cpus

--- read_proc.c
+++ read_proc.c
@@ -3,16 +3,22 @@
 #include<stdio.h>
 //#include "struct.h"
 #include"read_proc.h"
-
+char Name[] = "Name:";
+char Cpus_all[] = "Cpus_allowed:";
+char Cpus_all_list[] = "Cpus_allowed_list:";
+char mems_all[] = "Mems_allowed:";
+char mems_all_list[] = "Mem_allowed_list:";
+char Pid[] = "Pid:";
 void Get_Proc(char* Path)
 {
     int escape =1;
     char null[buffer],name[buffer],Cpus_allowed[buffer], Cpus_allowed_list[buffer];
+    char mems_allowed[100], mems_allowed_list[buffer],PID[buffer];
     char* pth = (char*)malloc(90*sizeof(unsigned char));
     strcpy(pth,Path);
     strcat(pth,"status");
 
-    printf("%s",pth);
+    printf("%s \n",pth);
     FILE* file = fopen(pth,"r");
     if(!file)
     {
@@ -24,18 +30,42 @@ void Get_Proc(char* Path)
         while(escape)
         {
             fscanf( file, "%s ", null);
-            if(null=="Name:")
+            if(strcmp(null,Name)==0)
             {
+                printf("%s",null);
                 fscanf(file,"%s",name);
+                printf("%s \n",name);
             }	
-            if(null=="Cpus_allowed:")
+            if(strcmp(null,Pid)==0)
             {
-                fscanf(file,"%s",Cpus_allowed);
+                printf("%s",null);
+                fscanf(file,"%s",PID);
+                printf("%s \n",PID);
             }
-            if(null=="Cpus_allowed_list:")
+            if(strcmp(null,Cpus_all)==0)
             {
+                printf("%s ",null);
+                fscanf(file,"%s",Cpus_allowed);
+                printf("%s\n",Cpus_allowed);
+            }
+            if(strcmp(null,Cpus_all_list)==0)
+            {
+                printf("%s",null);
                 fscanf(file,"%s",Cpus_allowed_list);
-                escape =0;
+                printf("%s \n",Cpus_allowed_list);
+            }
+            if(strcmp(null,mems_all)==0)
+            {
+                printf("%s",null);
+                fscanf(file,"%s",mems_allowed);
+                printf("%s \n",mems_allowed);
+            }
+            if(strcmp(null,mems_all_list)==0)
+            {
+                printf("%s",null);
+                fscanf(file,"%s",mems_allowed_list);
+                printf("%s \n",mems_allowed_list);
+                escape=0;
             }
         }
     }
