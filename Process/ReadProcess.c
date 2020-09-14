a8e477ad658d1f4ff9898f17d98dcbdd9ab6b2b2
tranngocson210599 <55520239+tranngocson210599@users.noreply.github.com>
2020-09-14 11:20:59 +0700

    Update ReadProcess.c

--- Process/ReadProcess.c
+++ Process/ReadProcess.c
@@ -28,7 +28,7 @@ void GetJob(char * path)
 			  
 	     //	    }
 	   }
-
+	      fclose(flie);
 	       //printf("Name : %s\nCpus_allowed : %s\nMems_allowed : %s \n",name,Cpus_allowed,Mems_allowed);
               printf("Name : %s\nCpus_allowed : %s\n",name,Cpus_allowed);
 	     
