1501b3d2c86230c8957d6d1e947578aba9652019
tranngocson210599 <55520239+tranngocson210599@users.noreply.github.com>
2020-09-14 09:24:52 +0700

    Update ReadProcess.h
    Rename function using Upper Case Camel naming convention

--- Process/ReadProcess.h
+++ Process/ReadProcess.h
@@ -3,6 +3,6 @@
 #include<stdio.h>
 #include<string.h>
 #include <ctype.h> // isdigit() function
-void get_job(char * path);//get info of one process
-void read_proc(void);     //get info of all process
+void GetJob(char * path);//get info of one process
+void ReadProc(void);     //get info of all process
 int is_uint(char input[]);//check input is a digit
