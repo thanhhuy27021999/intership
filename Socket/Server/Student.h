c09b70bdd9f8fdfa3df8f378cff8eec7a122594d
thanhhuy27021999 <thanhhuy27021999@gmail.com>
2020-09-23 13:12:48 +0700

    Init communication between client and server

---
+++ Socket/Server/Student.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"string.h"
+class Student
+{
+    private:
+        int Id;
+        char Name[20];
+        char Class[20];
+    public:
+        void Nhap();
+        void const Xuat();  
+};
